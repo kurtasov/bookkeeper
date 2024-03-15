67189c36c6668986970dab9c92bfc08ee6a1f95c
Lyudmila <ip.antonova62@gmail.com>
2024-03-15 21:43:10 +0300

    # test4

---
+++ bookkeeper/controller/controller.py
@@ -0,0 +1,30 @@
+from bookkeeper.models.entities import db
+import bookkeeper.controller.query_helper as qh
+
+
+class CrudController:
+    def __init__(self):
+        try:
+            db.bind(provider='sqlite', filename='../database.sqlite', create_db=True)  # TODO: Move DB file name to config / dotenv
+            db.generate_mapping(create_tables=True)
+        except Exception as e:
+            print(e)
+    def create(self, entity, params):
+        if entity == 'Budget':
+            qh.add_budget(monthly=params['monthly'], weekly=params['weekly'],
+                          daily=params['daily'])
+            return
+
+        raise NotImplementedError(f'Добавление для сущности {entity} не реализовано!')
+
+    def read(self, entity, params=None):
+        if entity == 'Budget':
+            return qh.get_budget()
+
+        raise NotImplementedError(f'Чтение для сущности {entity} не реализовано!')
+
+    def update(self, entity):
+        raise NotImplementedError(f'Изменение для сущности {entity} не реализовано!')
+
+    def delete(self, entity):
+        raise NotImplementedError(f'Удаление для сущности {entity} не реализовано!')
